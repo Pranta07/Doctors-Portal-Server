c11a237d2261512aa0f9aca8b6d89eaadac5375b
Pranta07 <palpranta@gmail.com>
2021-11-24 20:08:50 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,25 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("doctors-portal-server is running");
 });
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxq3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("database connected!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
